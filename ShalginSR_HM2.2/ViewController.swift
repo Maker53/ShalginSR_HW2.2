965a30f19d01dc5b0a60fbb2c5fda6c916c0a0f2
Maker53 <s.shalgin@gmail.com>
2021-10-23 09:36:30 +0300

    Initial Commit

---
+++ ShalginSR_HM2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShalginSR_HM2.2
+//
+//  Created by Станислав on 23.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
